1f4b250c28aa1116d5456eba9a7cff7ecbc93e29
christianarredondo <christianarredondo@utexas.edu>
2017-09-23 17:41:00 -0500

    set up node express server with Handlebars middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,18 +1,51 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
-app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Christian',
-    pets: 2,
-    favoriteFood: 'seafood'
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  let now = new Date();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.')
+    }
   });
+  next();
 });
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMessage: 'Welcome to this awesome website!'
+  });
+})
+
 app.get('/about', (req,res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+})
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 })
 
 app.get('/bad', (req,res) => {
